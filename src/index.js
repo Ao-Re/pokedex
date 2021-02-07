f39354fbc3dd3090aba703e845d9095295d251fd
Nadya <nadyaf.bachtiar@gmail.com>
2021-02-07 12:49:10 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import App from './component/App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
