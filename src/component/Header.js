abcd3821bf22cf89708b6236a10457e2ee82d71c
Nadya <nadyaf.bachtiar@gmail.com>
2021-02-07 10:58:56 +0700

    scroll fix

--- src/component/Header.js
+++ src/component/Header.js
@@ -63,8 +63,7 @@ const Header = () => {
     }
     
     const infiniteScroll = () => {
-        if (window.innerHeight + document.documentElement.scrollTop
-        === (document.documentElement.offsetHeight) && morePage){
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight  && morePage){
             console.log(window.innerHeight + document.documentElement.scrollTop);
             console.log(document.documentElement.offsetHeight);
             setIsFetching(true);
